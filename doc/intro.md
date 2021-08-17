c83590ddb1836c2b4b6fe44e79dce53df08e7b0e
Michael Wiederhold <michael.wiederhold@luminari.de>
2021-08-17 22:06:55 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to taskd-client-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/taskd_client_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns taskd-client-clj.core-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [taskd-client-clj.core :as sut]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
