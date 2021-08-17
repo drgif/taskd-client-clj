36b840339872509d4373b6186f6abac08b27c36e
Michael Wiederhold <michael.wiederhold@luminari.de>
2021-08-17 22:47:11 +0200

    Adds rudimentary message builder

--- src/taskd_client_clj/core.clj
+++ src/taskd_client_clj/core.clj
@@ -1,6 +1,28 @@
 (ns taskd-client-clj.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def client-info 
+  (str
+   "taskd-client-clj "
+   (System/getProperty "taskd-client-clj.version")))
+
+(def protocol-version "v1")
+
+(def crlf \u000d)
+
+(defn build-message
+  [type {:keys [organization user password sync-key payload]}]
+  (str "<<size>>"
+       "type: " type crlf
+       "org: " organization crlf
+       "user: " user crlf
+       "key: " password crlf
+       "client: " client-info crlf
+       "protocol: " protocol-version crlf
+       crlf))
+
+(comment
+  client-info
+  crlf
+  (build-message "sync" {:organization "Home"
+                         :user "test"
+                         :password "test-pw"}))
