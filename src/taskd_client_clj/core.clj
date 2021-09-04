664aadf574fbf882eb4bea0f39262fdda3e2eef0
Michael Wiederhold <michael.wiederhold@luminari.de>
2021-09-04 23:27:26 +0200

    Adds basic messages

--- src/taskd_client_clj/core.clj
+++ src/taskd_client_clj/core.clj
@@ -9,20 +9,38 @@
 
 (def crlf \u000d)
 
-(defn build-message
-  [type {:keys [organization user password sync-key payload]}]
-  (str "<<size>>"
-       "type: " type crlf
-       "org: " organization crlf
-       "user: " user crlf
-       "key: " password crlf
-       "client: " client-info crlf
-       "protocol: " protocol-version crlf
-       crlf))
+(defn- build-message
+  [type payload {:keys [organization user password]}]
+  (let [message (str "type: " type crlf
+                     "org: " organization crlf
+                     "user: " user crlf
+                     "key: " password crlf
+                     "client: " client-info crlf
+                     "protocol: " protocol-version crlf
+                     crlf
+                     payload)]
+    (str ;(+ 4 (alength (.getBytes message))) 
+         message)))
 
+(defn sync-message
+  [tasks sync-key auth]
+  (build-message "sync"
+                 (reduce #(str %1 %2 crlf) sync-key tasks)
+                 auth))
+
+(defn statistics-message
+  [auth]
+  (build-message "statistics"
+                 nil
+                 auth))
+
+(def test-auth {:organization "Home"
+                :user "test"
+                :password "test-pw"})
+  
 (comment
   client-info
   crlf
-  (build-message "sync" {:organization "Home"
-                         :user "test"
-                         :password "test-pw"}))
+  (build-message "sync" {} test-auth)
+  (sync-message [{:task 1} {:task 2}] "123456" test-auth)
+  (statistics-message test-auth))
